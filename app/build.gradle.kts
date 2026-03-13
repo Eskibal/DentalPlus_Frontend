ee05ade93ce2c708039dafcd82437a38c35600fa
Eskibal <jeanpatrick.esquibal@gmail.com>
2026-03-13 15:38:30 +0100

    Configuración de AndroidManifest.xml y dependencias en build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,9 +38,9 @@ android {
 }
 
 dependencies {
-    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
-    implementation("com.squareup.okhttp3:okhttp:4.11.0")
-    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation ("com.squareup.retrofit2:retrofit:3.0.0")
+    implementation("com.squareup.okhttp3:okhttp:5.3.2")
+    implementation ("com.squareup.retrofit2:converter-gson:3.0.0")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
